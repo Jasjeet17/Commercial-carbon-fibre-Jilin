ff23ec6aaca31a0d4d3a538b2a40bca49ac93d46
Kaur <kau019@csiro.au>
2019-10-17 13:35:02 +1100

    My first ever plot for experiment1

--- Script/Script Jilin.R
+++ Script/Script Jilin.R
@@ -1,33 +1,26 @@
-# load the library tidyverse
 library(tidyverse)
-# load the library excel
 library(readxl)
-#load library cowplot
 library(cowplot)
-#read datafile into excel
-read_excel("Data/Jilin_carbonisation_July_2019JK.xlsx")
-#Asking it to read first sheet of excel file
 Jilin_carbonisation_data_july2019 <- read_excel("Data/Jilin_carbonisation_July_2019JK.xlsx", sheet=1)
-# Now I would like to read in a messy excel file and will try to tidy it
-Jilin_messy_excel <- read_excel("Data/Jilin_carbonisation_July_2019JK.xlsx", sheet= "July Trial cond")
-# From the tidy data, I want to select only experiment 1 and plot strength for all zones
+str(Jilin_carbonisation_data_july2019)
+# From the data, I want to select only experiment 1 and plot strength for all zones
 Experiment1 <- Jilin_carbonisation_data_july2019 %>%
   filter(Experiment==1)
-  Experiment1
-  view(Experiment1)
+Experiment1
+view(Experiment1)
 # I want to select two columns, zones and break.stress
-  
-    Strength1 <- Experiment1 %>%
-    select(Zones, Break.Stress) %>%
-    rename('Tensile Strength'= Break.Stress) %>%
-    filter(!is.na('Tensile Strength')) #do not include NA values
-    view(Strength1)
-# I want to plot the above
-   
-Strengthplot1 <- ggplot(data=Strength1, mapping=aes (x=Zones, y=Break.Stress)) + geom_point() 
+Strength1 <- Experiment1 %>%
+  select(Zones, Tensile_Strength) 
+Strength2 <- drop_na(Strength1) #do not include NA values
+view(Strength2)
+str(Strength2)
+
+# Recode your variables
+Strength2$Zones<-as.factor(Strength2$Zones)
+Strength2$Tensile_Strength<- as.numeric(Strength2$Tensile_Strength)
+str(Strength2)
+Strength3 <- Strength2 %>%
+  mutate(Zones=fct_relevel(Zones,"Zone 1","Zone 2","Zone 3", "Zone 4", "LT 3", "HT2","Winder"))
+Strengthplot1 <- ggplot(Strength3, aes(x=Zones, y=Tensile_Strength)) + geom_jitter(width=0.1) 
 Strengthplot1    
-    # how do I filter out NA from experiment1
-# how do i remove lt1,lt2 and ht1 from datatable?
-    # how do I have zones in sequence in x-axis
-    # why I am getting so much noise in the left end of graph
-  #how can i rename break stress to tensile strength
+
